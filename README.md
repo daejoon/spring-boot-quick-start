a1f3384f90fb026cad141c187a05b246518753f4
daejoon <kkode1911@gmail.com>
2019-11-17 14:52:48 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring Boot Quick Start
+[Blog - Spring Boot Quick Start](https://blog.ddoong2.com/2019/11/11/Spring-Boot-Quick-Start/)
---
+++ src/main/java/com/ddoong2/springbootquickstart/App.java
@@ -0,0 +1,20 @@
+package com.ddoong2.springbootquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Spring Boot Quick Start";
+    }
+}
